253fed96087c1141e9681d3fb53a8992d28b7ba3
xjw92 <jiaweixiao040292@tamu.edu>
2015-09-23 21:54:06 -0500

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +11,37 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all.pluck(:rating).uniq
+    
+    if !params[:sort_col].nil?
+      @sorted_col = params[:sort_col]
+      session[:sort_col] = params[:sort_col]
+    elsif !session[:sort_col].nil?
+      @sorted_col = session[:sort_col]
+    else
+      @sorted_col = nil
+    end
+
+    if !params[:ratings].nil?
+      @selected_ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif params[:ratins].nil? and params[:commit] == "Refresh"
+      @selected_ratings = nil
+      session[:ratings] = nil
+    elsif !session.nil?
+      @selected_ratings = session[:ratings]
+    else
+      @selected_ratings = nil
+    end
+
     @movies = Movie.all
+
+    if !@selected_ratings.nil?
+      @movies = @movies.select {|m| @selected_ratings.include? m.rating}    
+    end     
+    if !@sorted_col.nil?
+      @movies = @movies.sort_by {|hsh| hsh[@sorted_col.to_sym]}
+    end         
   end
 
   def new
@@ -15,7 +49,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -26,7 +60,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
